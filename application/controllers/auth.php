80d8d36c99eb711c062bf2527f8a58c1b304ed25
YusufakbarWeb <yusufakbarp12@gmail.com>
2020-11-14 20:41:01 +0700

    membuat login

---
+++ application/controllers/auth.php
@@ -0,0 +1,39 @@
+<?php
+
+class Auth extends CI_Controller
+{
+    public function login()
+    {
+        $this->form_validation->set_rules('username', 'Username', 'required', ['required' => 'Username Harus Diisi']);
+        $this->form_validation->set_rules('password', 'Password', 'required', ['required' => 'Password Harus Diisi']);
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('templates/header');
+            $this->load->view('form_login');
+            $this->load->view('templates/footer');
+        } else {
+            $auth = $this->model_auth->cek_login();
+            if ($auth == FALSE) {
+                $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                Username atau Password Anda Salah!!!
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>');
+                redirect('auth/login');
+            } else {
+                $this->session->set_userdata('username', $auth->username);
+                $this->session->set_userdata('role_id', $auth->role_id);
+                switch ($auth->role_id) {
+                    case 1:
+                        redirect('admin/dashboard_admin');
+                        break;
+                    case 2:
+                        redirect('dashboard');
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ application/views/form_login.php
@@ -0,0 +1,51 @@
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-5 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">LOGIN</h1>
+                                    </div>
+                                    <?php echo $this->session->flashdata('pesan'); ?>
+                                    <form method="POST" action="<?php echo base_url('auth/login'); ?>" class="user">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control form-control-user" id="username" placeholder="Username" name="username">
+                                            <?php echo form_error('username', '<div class="text-danger small">', '</div>'); ?>
+
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password" name="password">
+                                            <?php echo form_error('password', '<div class="text-danger small">', '</div>'); ?>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary form-control">LOGIN</button>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="register.html">Buat Akun Baru</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+</body>
+
+</html>
