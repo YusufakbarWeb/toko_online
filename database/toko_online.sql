c439ad3520d199f205f381e9985677175d27e1d2
YusufakbarWeb <yusufakbarp12@gmail.com>
2020-11-15 11:06:02 +0700

    update db"

--- database/toko_online.sql
+++ database/toko_online.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 13, 2020 at 12:36 PM
+-- Generation Time: Nov 15, 2020 at 05:04 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.2
 
@@ -43,10 +43,31 @@ CREATE TABLE `tb_barang` (
 --
 
 INSERT INTO `tb_barang` (`id_brg`, `nama_brg`, `keterangan`, `kategori`, `harga`, `stok`, `gambar`) VALUES
-(1, 'Sepatu All Star', 'Sepatu untuk pria\r\ncocok untuk berpergian', 'Sepatu', 250000, 5, 'sepatu1.jpg'),
+(1, 'Sepatu All bintang', 'Sepatu untuk pria', 'Sepatu', 500000, 6, 'sepatu1.jpg'),
 (2, 'Kemeja Batik', 'Kemeja Pria Lengan Panjang Dengan Kombinasi Batik', 'Pakaian', 70000, 10, 'kemeja1.jpg'),
-(3, 'Kamera SONY Cybershot DSC-H300 ', 'Kamera yang canggih dan kekinian', 'Elektronik', 2800000, 2, 'kamera1.jpg'),
-(40, 'Vivo Y30', 'hp terbaru', 'Elektronik', 2000000, 8, 'vivo.jpg');
+(3, 'Kamera SONY Cybershot DSC-H300 ', 'Kamera yang canggih dan kekinian', 'Elektronik', 2800000, 2, 'kamera1.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `role_id` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`id`, `nama`, `username`, `password`, `role_id`) VALUES
+(1, 'admin', 'admin', 'admin', 1),
+(3, 'user', 'user', 'user', 2);
 
 --
 -- Indexes for dumped tables
@@ -59,6 +80,12 @@ ALTER TABLE `tb_barang`
   ADD PRIMARY KEY (`id_brg`);
 
 --
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -66,7 +93,13 @@ ALTER TABLE `tb_barang`
 -- AUTO_INCREMENT for table `tb_barang`
 --
 ALTER TABLE `tb_barang`
-  MODIFY `id_brg` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `id_brg` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
