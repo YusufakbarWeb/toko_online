51472ac86de097950d4e16e6abd03a3bce580455
YusufakbarWeb <yusufakbarp12@gmail.com>
2020-11-15 10:34:00 +0700

    memperbaiki login

--- application/views/templates_admin/sidebar.php
+++ application/views/templates_admin/sidebar.php
@@ -107,10 +107,20 @@
 
                         <ul class="na navbar-nav navbar-right">
                             <?php if ($this->session->userdata('username')) { ?>
-                                <li>
-                                    <div>Selamat Datang <?php echo $this->session->userdata('username') ?></div>
+                                <li class="nav-item dropdown no-arrow">
+                                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $this->session->userdata('username') ?></span>
+                                        <img class="img-profile rounded-circle" src="<?php echo base_url('assets/img/admin.png') ?>">
+                                    </a>
+                                    <!-- Dropdown - User Information -->
+                                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="" data-toggle="modal" data-target="#logoutModal">
+                                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                            Logout
+                                        </a>
+                                    </div>
                                 </li>
-                                <li class="ml-2"><?php echo anchor('auth/logout', 'logout') ?></li>
                             <?php } else { ?>
                                 <li><?php echo anchor('auth/login', 'login') ?></li>
                             <?php } ?>
